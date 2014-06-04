3cf4135fcfc45cd1f957bd953ef379a75d00711f
Danielle Mayne <danielle.mayne@enstratus.com>
2014-06-04 17:00:49 +0100

    bugz 4626: set normal priority shares for cpu and memory

--- src/main/java/org/dasein/cloud/vcloud/vCloud.java
+++ src/main/java/org/dasein/cloud/vcloud/vCloud.java
@@ -21,6 +21,7 @@ package org.dasein.cloud.vcloud;
 import org.apache.log4j.Logger;
 import org.dasein.cloud.AbstractCloud;
 import org.dasein.cloud.CloudException;
+import org.dasein.cloud.ContextRequirements;
 import org.dasein.cloud.ProviderContext;
 import org.dasein.cloud.util.APITrace;
 import org.dasein.cloud.vcloud.compute.vCloudComputeServices;
@@ -103,6 +104,14 @@ public class vCloud extends AbstractCloud {
         String name = (ctx == null ? null : ctx.getCloudName());
 
         return (name == null ? "Private vCloud Cloud" : name);
+    }
+
+    @Override
+    public @Nonnull
+    ContextRequirements getContextRequirements() {
+        return new ContextRequirements(
+                new ContextRequirements.Field("apiKey", "The API Keypair", ContextRequirements.FieldType.KEYPAIR, ContextRequirements.Field.ACCESS_KEYS, true)
+        );
     }
 
     @Override
